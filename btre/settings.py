d6282fc13a2270fd4c4d7dfef97695c16ae3e56c
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-02-05 13:34:03 +0100

    Added local settings imports

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'uniqueomokenny@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
